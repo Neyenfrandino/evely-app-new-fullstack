4f34d2319f9735092ffa359c3a3bef6c329fc437
Neyen Frandino <neyenfrandino1@gmail.com>
2024-09-06 16:38:56 -0300

    funcioncines baicas CRUD en users

--- BACKEND/app/repository/user_repository.py
+++ BACKEND/app/repository/user_repository.py
@@ -1,7 +1,7 @@
 # Aqui van a ir todas las funciones que se necesiten para el backend
 from app.db.models import User
 
-def create_user( schema, db):
+def create_user(schema, db):
     user_dict = dict(schema)
 
     user_true = db.query(User).filter(User.email == user_dict["email"]).first()
@@ -13,3 +13,35 @@ def create_user( schema, db):
         return {"message": "User created successfully"}
     else:
         return {"message": "User already exists"}
+
+def read_users(user_id, db):
+    user_true = db.query(User).filter(User.id == user_id).first()
+
+    if user_true is None:
+        return {"message": "User not found"}
+    
+    return user_true
+
+def update_user(user_id, schema, db):
+    user_dict = dict(schema)
+    user_true = db.query(User).filter(User.id == user_id).first()
+    print(user_dict)
+    if user_true is None:
+        return {"message": "User not found"}
+    else:
+        for key, value in user_dict.items():
+            if value != 'string' and value != '' : 
+                setattr(user_true, key, value)
+
+        db.add(user_true)
+        db.commit()
+        return {"message": "User updated successfully"}
+    
+def delete_user(user_id, db):
+    user_true = db.query(User).filter(User.id == user_id).first()
+    if user_true is None:
+        return {"message": "User not found"}
+    else:
+        db.delete(user_true)
+        db.commit()
+        return {"message": "User deleted successfully"}
--- BACKEND/app/routers/user/user.py
+++ BACKEND/app/routers/user/user.py
@@ -5,22 +5,27 @@ from fastapi import APIRouter, Depends
 from app.schemas.schemas import Schema_user_post
 from app.db.database import get_db
 # from app.db.models import User
-from app.repository.user_repository import create_user
+from app.repository.user_repository import create_user, read_users, update_user, delete_user
 
 router = APIRouter(prefix=f"/user", tags=["user"])
 
-
 @router.post("/create_user")
-def read_users(schema_user: Schema_user_post, db: Session = Depends(get_db)):
+def create_users(schema_user: Schema_user_post, db: Session = Depends(get_db)):
     # Convertir el esquema en un diccionario
     response = create_user( schema_user, db)
     return response
 
+@router.get("/read_user")
+def read_users_id(user_id:int, db: Session = Depends(get_db)):
+    response = read_users(user_id, db)
+    return response 
 
-@router.get("/")
-def read_users(db: Session = Depends(get_db)):
-    # Aqui se puede hacer cualquier consulta a la base de datos 
-    # Por ejemplo, si queremos obtener todos los usuarios de la base de datos
-    # users = db.query(User).all()
-    # return users
-    pass
+@router.patch("/update_user")
+def update_users(user_id:int, schema_user: Schema_user_post, db: Session = Depends(get_db)):
+    response = update_user(user_id, schema_user, db)
+    return response
+
+@router.delete("/delete_user")
+def delete_users(user_id:int, db: Session = Depends(get_db)):
+    response = delete_user(user_id, db)
+    return response
